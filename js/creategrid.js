18ff4fd198115c28a2c81d41954e5ca801556582
Joseph Van Geffen <geffen@gmail.com>
2012-04-18 01:51:38 -0700

    Fixed grid some, css tweaks

--- js/creategrid.js
+++ js/creategrid.js
@@ -5,27 +5,29 @@ var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],
 //var samplesize = Math.round(x/16) * Math.round((y-60)/16); //icons are 16x16, 60 = nav height
 
 //table, divs, canvas?
+function randOrd(){ 
+	return (Math.round(Math.random())-0.5); 
+	}
+	
+	
 
 if (!minegrid) {
 var minegrid = [];
 var gridlocation = [];
 var iconsize = 32;
 
-for (var i = 0; i < Math.floor((y-120)/iconsize); i++) {
+for (var i = 0; i < Math.floor((y)/iconsize); i++) {
    //iterate through rows
-   //rows would be accessed using the "row" variable assigned in the for loop
-   for (var j = 0;j < Math.floor((x-24)/iconsize); j++) {
+   for (var j = 0;j < Math.floor((x)/iconsize); j++) {
      //iterate through columns
-     //columns would be accessed using the "col" variable assigned in the for loop
 	 gridlocation =[Number(i)+"-"+Number(j)];
 	 minegrid.push(gridlocation);
 	 
 	 $('#good').append('<div id="' + gridlocation + '" class="square"></div>');
-	 //$('.0-1').fadeOut();
+	
    }  
 }
 
-function randOrd(){ return (Math.round(Math.random())-0.5); }
 minegrid.sort( randOrd );
 }
-//alert(minegrid[900]);
+
